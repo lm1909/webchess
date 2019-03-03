8e1f0281bdfe12804d8ba47b9580ff067cbff23d
lm1909 <cyteroxsoftware@gmail.com>
2019-03-03 23:13:40 +0100

    Re-added the nick text field

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -259,6 +259,7 @@ instance YesodAuth App where
             Nothing -> Authenticated <$> insert User
                 { userIdent = credsIdent creds
                 , userPassword = Nothing
+                , userNick = Nothing
                 }
 
     -- You can add other plugins like Google Email, email or OAuth here
