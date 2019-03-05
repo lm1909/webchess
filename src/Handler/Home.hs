a48dd5b877a769649be83947857e056351b2bfd2
lm1909 <cyteroxsoftware@gmail.com>
2019-03-05 02:02:57 +0100

    Cleaned up Home.hs

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,22 +9,9 @@ import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-                setTitle "Welcome To Yesod!"
+                setTitle "webchess - Home"
                 $(widgetFile "homepage")
 
 
