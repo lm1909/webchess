6f4646f205f4e30ac3d1f4636935e44a57eda462
lm1909 <cyteroxsoftware@gmail.com>
2019-03-05 02:01:58 +0100

    Profile with message, nickChange

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -3,14 +3,34 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Handler.Profile where
 
 import Import
+
+data AccountData = AccountData {nick :: Text} deriving Show
+
+accountForm :: Maybe Text -> Form AccountData
+accountForm maybename = renderBootstrap $ AccountData
+                <$> areq textField "Nickname: " maybename
 
 getProfileR :: Handler Html
 getProfileR = do
     (id, user) <- requireAuthPair
     person <- runDB $ get404 id
+    (widget, enctype) <- generateFormPost (accountForm (userNick person))
     defaultLayout $ do
-        setTitle . toHtml $ userIdent user <> "'s User page"
+        setTitle "User page"
         $(widgetFile "profile")
+
+postProfileR :: Handler Html
+postProfileR = do ((result, widget), enctype) <- runFormPost (accountForm Nothing)
+
+                  (id, user) <- requireAuthPair
+                  (person) <- runDB $ get id
+
+                  case result of
+                    FormSuccess account -> do runDB $ update id [UserNick =. (Just $ nick account)]
+                                              setMessage $ toHtml ("Updated User nickname" :: Text)
+                                              defaultLayout $ $(widgetFile "profile")
+                    _ -> defaultLayout $ $(widgetFile "profile")
