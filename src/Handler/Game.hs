99b867bc6b8f4b34ff2dfad649fd108012f6e9b2
lm1909 <cyteroxsoftware@gmail.com>
2019-03-07 21:07:18 +0100

    Added game account authorisation

--- src/Handler/Game.hs
+++ src/Handler/Game.hs
@@ -15,6 +15,7 @@ import Logic.ChessLegal
 import Data.Text as DT
 import Text.Julius (RawJS (..))
 import Logic.Chess
+import Control.Lens
 
 data MoveForm = MoveForm {ox :: Int, oy :: Int, dx :: Int, dy :: Int}
 
@@ -45,6 +46,7 @@ gameToChessData game = gameFromMoves (textToHistory $ gameHistory game)
 
 getGameR :: GameId -> Handler Html
 getGameR gameId = do game <- runDB $ get404 gameId
+                     (id, user) <- requireAuthPair
                      ((res, movewidget), enctype) <- runFormGet moveForm
                      let cd = gameToChessData game
                      (player, opponent) <- runDB $ do player <- get404 (gamePlayer game) -- @TODO is a 404 really optimal here?
@@ -58,19 +60,26 @@ getGameR gameId = do game <- runDB $ get404 gameId
 postGameR :: GameId -> Handler Html
 postGameR gameId = do ((result, widget), enctype) <- runFormPostNoToken moveForm -- @TODO enable cross site request forgery protection
                       game <- runDB $ get404 gameId
+                      (id, user) <- requireAuthPair
+                      (player, opponent) <- runDB $ do player <- get404 (gamePlayer game) -- @TODO is a 404 really optimal here?
+                                                       opponent <- get404 (gameOpponent game)
+                                                       return (player, opponent)
                       let cd = gameToChessData game
-                      case result of
-                        FormSuccess (MoveForm ox oy dx dy) -> do let move = Move (1 + (Prelude.length $ _history $ cd)) (ox,oy) (dx,dy) 
-                                                                 case makeMove move cd of 
-                                                                    Valid cd' -> do runDB $ do update gameId [GameHistory =. (historyToText $ (_history cd'))]
-                                                                                               update gameId [GameGameStatus =. (_status cd')]
-                                                                                    redirect (GameR gameId)
-                                                                    Invalid r -> do setMessage $ toHtml ("Move invalid: " Prelude.++ show r)
-                                                                                    redirect (GameR gameId)
-                        FormFailure f -> do setMessage $ toHtml ("Failure " Prelude.++ show f)
-                                            redirect (GameR gameId)
-                        FormMissing -> do setMessage $ toHtml ("Form Missing" :: Text)
-                                          redirect (GameR gameId)
+                      case (((id == gamePlayer game) && (cd^.playerOnTurn == White)) || ((id == gameOpponent game) && (cd^.playerOnTurn == Black))) of
+                        True -> case result of
+                                        FormSuccess (MoveForm ox oy dx dy) -> do let move = Move (1 + (Prelude.length $ _history $ cd)) (ox,oy) (dx,dy) 
+                                                                                 case makeMove move cd of 
+                                                                                    Valid cd' -> do runDB $ do update gameId [GameHistory =. (historyToText $ (_history cd'))]
+                                                                                                               update gameId [GameGameStatus =. (_status cd')]
+                                                                                                    redirect (GameR gameId)
+                                                                                    Invalid r -> do setMessage $ toHtml ("Move invalid: " Prelude.++ show r)
+                                                                                                    redirect (GameR gameId)
+                                        FormFailure f -> do setMessage $ toHtml ("Failure " Prelude.++ show f)
+                                                            redirect (GameR gameId)
+                                        FormMissing -> do setMessage $ toHtml ("Form Missing" :: Text)
+                                                          redirect (GameR gameId)
+                        False -> do setMessage $ toHtml ("Not authorized to make this move" :: Text)
+                                    redirect (GameR gameId)
 
 renderSquare :: (Int, Int) -> Square -> Widget
 renderSquare (x, y) e = case e of 
