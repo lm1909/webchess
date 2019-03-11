18242599f1468570d9749b5c413e42004b055e96
lm1909 <cyteroxsoftware@gmail.com>
2019-03-11 01:04:51 +0100

    Cleanup; no shows AI games under running games as well

--- src/Handler/Running.hs
+++ src/Handler/Running.hs
@@ -1,25 +1,30 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Handler.Running where
 
-import Import
-import Logic.ChessData
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import           Import
+import           Logic.ChessData
+
 import           Database.Persist
 import           Database.Persist.Sqlite
-import           Database.Persist.TH
 
 getRunningR :: Handler Html
 getRunningR = do runninggames <- runDB $ selectList [GameGameStatus ==. Running] []
+                 runningaigames <- runDB $ selectList [AiGameGameStatus ==. Running] []
                  defaultLayout $ do setTitle "Running games"
                                     $(widgetFile "running")
 showGame :: Entity Game -> String
 showGame (Entity key game) = "Game #" ++ (show $ unSqlBackendKey $ unGameKey key) ++ " with Status " ++ (show $ gameGameStatus game)
--- Game GameKey {unGameKey = SqlBackendKey {unSqlBackendKey = 3}}
 
--- keyIdGame :: Entity Game -> GameId
+keyIdGame :: Entity Game -> GameId
 keyIdGame (Entity key _) = key
+
+showAiGame :: Entity AiGame -> String
+showAiGame (Entity key aigame) = "AI Game #" ++ (show $ unSqlBackendKey $ unAiGameKey key) ++ " with Status " ++ (show $ aiGameGameStatus aigame)
+
+keyIdAiGame :: Entity AiGame -> AiGameId
+keyIdAiGame (Entity key _) = key
